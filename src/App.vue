768d8442357202ebf9078872e7ab11f7f18c4498
BlueStar007 <gtx_2048@outlook.com>
2020-08-14 19:50:42 +0800

    bom完善

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <div id="app">
-
+      <Bom></Bom>
   </div>
 </template>
 
 <script>
 import{Toast} from 'mint-ui'
+import Bom from './components/Bom'
 export default {
   name: 'App',
   components: {
-
+      Bom,
   },
   mounted(){
     Toast('nihao')
---
+++ src/components/Bom.vue
@@ -0,0 +1,40 @@
+<template>
+        <div class="con">
+            <mt-tabbar v-model="selected">
+            <mt-tab-item id="tab1">
+                <img slot="icon" src="">
+                首页
+            </mt-tab-item>
+            <mt-tab-item id="tab2">
+                <img slot="icon" src="">
+                分类
+            </mt-tab-item>
+            <mt-tab-item id="tab3">
+                <img slot="icon" src="">
+                购物车
+            </mt-tab-item>
+            <mt-tab-item id="tab4">
+                <img slot="icon" src="">
+                我的
+            </mt-tab-item>
+            </mt-tabbar>            
+        </div>
+</template>
+
+<script>
+ import Vue from 'vue'
+ import {Tabbar, TabItem} from 'mint-ui'
+ Vue.component(Tabbar.name, Tabbar);
+ Vue.component(TabItem.name, TabItem);
+export default {
+       data() {
+           return {
+               selected: 'tab1'
+           }
+       },
+}
+</script>
+
+<style scoped>
+
+</style>
