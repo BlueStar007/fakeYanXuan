e22d79a7ff8b279379e1d2b47a858f929ea0dca0
BlueStar007 <gtx_2048@outlook.com>
2020-08-14 08:33:27 +0800

    引入了mini-ui

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
-    
+
   </div>
 </template>
 
 <script>
-
+import{Toast} from 'mint-ui'
 export default {
   name: 'App',
   components: {
 
+  },
+  mounted(){
+    Toast('nihao')
   }
 }
 </script>
